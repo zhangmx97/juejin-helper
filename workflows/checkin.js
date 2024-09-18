7cdcff7d80c263af3b4a169465845ead27684f4a
zhangmx97 <849325634@qq.com>
2024-09-18 09:37:31 +0800

    cancel diplucky task

--- workflows/checkin.js
+++ workflows/checkin.js
@@ -254,8 +254,8 @@ class CheckIn {
     await this.sdkTask.run();
     console.log(`运行 ${this.growthTask.taskName}`);
     await this.growthTask.run();
-    console.log(`运行 ${this.dipLuckyTask.taskName}`);
-    await this.dipLuckyTask.run();
+    // console.log(`运行 ${this.dipLuckyTask.taskName}`);
+    // await this.dipLuckyTask.run();
     console.log(`运行 ${this.lotteriesTask.taskName}`);
     await this.lotteriesTask.run(this.growthTask, this.dipLuckyTask);
     console.log(`运行 ${this.bugfixTask.taskName}`);
